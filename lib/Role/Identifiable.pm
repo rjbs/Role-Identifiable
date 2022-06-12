0bc903f36e8aeb13224bf2b92eb5fc4a5e0bede6
Ricardo Signes <rjbs@semiotic.systems>
2022-06-12 15:20:21 -0400

    Role::Identifiable: just a stupid module for the indexer

---
+++ lib/Role/Identifiable.pm
@@ -0,0 +1,18 @@
+package Role::Identifiable;
+# ABSTRACT: a thing you can identify somehow
+
+use strict;
+use warnings;
+
+=head1 DESCRIPTION
+
+Role::Identifiable isn't really a module that does anything.  It's here to make
+things simpler for indexing on CPAN and looking up docs.
+
+You probably want to use either L<Role::Identifiable::HasIdent>, for
+identifying things by an identifier string, or L<Role::Identifiable::HasTags>
+for identifying things by a list of tags.
+
+=cut
+
+1;
